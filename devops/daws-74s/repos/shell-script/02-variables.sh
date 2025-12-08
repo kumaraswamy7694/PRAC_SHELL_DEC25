70882823a4e76dc372c55f8ed0b2ecc1b882b32a
saik90305@gmail.com <saik90305@gmail.com>
2025-12-08 15:36:44 +0530

    added

--- devops/daws-74s/repos/shell-script/02-variables.sh
+++ devops/daws-74s/repos/shell-script/02-variables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DATE=$(date)
 TIME=$(time)
-echo "Hello, this script executed at time: $DATE and $TIME"
+echo "Hello, this script executed at time: $DATE"
+echo "hello this is performed at time: $TIME"
