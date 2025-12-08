28d56466763c3516eec6a2ad1dd8eb9922e5b86f
saik90305@gmail.com <saik90305@gmail.com>
2025-12-08 16:29:57 +0530

    added

---
+++ devops/daws-74s/repos/shell-script/06-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#this is called array instead of single value
+PERSONS=("sai" "vijay" "ram" "shyam")
+echo "First persion is : ${PERSONS[0]}"
+echo "Second persion is : ${PERSONS[1]}"
+echo "Third persion is : ${PERSONS[2]}"  
