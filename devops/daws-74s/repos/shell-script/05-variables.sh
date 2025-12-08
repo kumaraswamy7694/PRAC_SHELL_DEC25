67ddc6218d5cba07d6d5b1228015902908526dc1
saik90305@gmail.com <saik90305@gmail.com>
2025-12-08 16:03:56 +0530

    added

--- devops/daws-74s/repos/shell-script/05-variables.sh
+++ devops/daws-74s/repos/shell-script/05-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo  "please enter your user name"
-read USERNAME #this is the variable where the value of the user entered
+read USERNAME
+read AGE #this is the variable where the value of the user entered
 echo "you have entered user name as: $USERNAME"
 
 echo " hello $USERNAME, welcome to devops withe aws"
+echo "you are $AGE years old"
+echo " you are elible for voting"  #this is a comment
 echo " how are you mr $USERNAME today?"
