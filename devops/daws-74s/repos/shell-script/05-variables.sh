06f8b6a5133c3c43b124a8379b91d8f285cd967d
saik90305@gmail.com <saik90305@gmail.com>
2025-12-08 16:18:17 +0530

    added

--- devops/daws-74s/repos/shell-script/05-variables.sh
+++ devops/daws-74s/repos/shell-script/05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo " please enter your name "
-read NAME
+read -s NAME
 echo " your enter name is : $NAME "
 echo " enter your password "
-read PASSWORD
+read -s PASSWORD
 echo " your entered password is : $PASSWORD "
