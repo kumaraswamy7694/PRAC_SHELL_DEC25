4ca3d64beccc8adab26313bb689b2737de3e7e6c
saik90305@gmail.com <saik90305@gmail.com>
2025-12-10 16:21:31 +0530

    added

--- devops/daws-74s/repos/shell-script/09-functions.sh
+++ devops/daws-74s/repos/shell-script/09-functions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-USERID=$(id -u) # executing the id -u and  saving in USERID variable
-# validate function should previous command and inform user it is success ofr failure
+
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -12,14 +14,18 @@ VALIDATE(){
     fi
 
 }
+USERID=$(id -u) # executing the id -u and  saving in USERID variable
+# validate function should previous command and inform user it is success ofr failure
+
+
 if [ $USERID -ne 0 ]
 then 
     echo " ERROR :: please run with sudo or root user"
     exit 1  # command will exit with status 1 because it is not sudo 
 fi
 
-yum install mysql -y 
+yum install mysql -y &>> $LOGFILE
 VALIDATE $? "installing mysql"
 
-yum install postfix -y
+yum install postfix -y &>> $LOGFILE
 VALIDATE $? "installing postfix"
