120076a3c5742dc0f78384f5e147e9ad8e1a3b8e
saik90305@gmail.com <saik90305@gmail.com>
2025-12-10 11:44:52 +0530

    added

--- devops/daws-74s/repos/shell-script/06-array.sh
+++ devops/daws-74s/repos/shell-script/06-array.sh
@@ -3,4 +3,4 @@
 PERSONS=("sai" "vijay" "ram" "shyam")
 echo "First persion is : ${PERSONS[0]}"
 echo "Second persion is : ${PERSONS[1]}"
-echo "Third persion is : ${PERSONS[2]}"  
+echo "Third persion is : ${PERSONS[2]}"    
