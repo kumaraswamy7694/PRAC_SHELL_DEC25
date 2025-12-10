259a0798724aeab50b28458555d4e8a0da9079cc
saik90305@gmail.com <saik90305@gmail.com>
2025-12-10 12:10:08 +0530

    added

--- devops/daws-74s/repos/shell-script/07-conditions.sh
+++ devops/daws-74s/repos/shell-script/07-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #check if the number is greater that 10 or not
 NUM=$1
-if [ $NUM -gt 10]  #if starting the conditon
+if [ $NUM -gt 10 ]  #if starting the conditon
 then 
     echo " $NUM is greater than 10"
 else
-    echo "$NUM is less than or equal to 10"
+    echo "$NUM is less than  "
 fi # ending the conditon 
